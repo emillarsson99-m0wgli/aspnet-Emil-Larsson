9f0eb293d9d87b97dd1acb3cd5e01667ba056149
Emil L <emillarsson99@gmail.com>
2026-04-27 14:14:18 +0200

    Created WorkoutClass entity

---
+++ CoreFitnessClub.Domain/Entities/WorkoutClass.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreFitnessClub.Domain.Entities;
+
+public class WorkoutClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public DateTime Date { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public string Instructor { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public int Capacity { get; set; }
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+}
