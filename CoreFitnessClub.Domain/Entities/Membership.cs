4a4f6fe7ab8e0eb0ab422d7d27eb966a3ff96548
Emil L <emillarsson99@gmail.com>
2026-04-27 14:12:06 +0200

    Created Membership entity

---
+++ CoreFitnessClub.Domain/Entities/Membership.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreFitnessClub.Domain.Entities;
+
+public class Membership
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = null!;
+    public string MembershipType { get; set; } = null!;
+    public string Status { get; set; } = "Active";
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+}
