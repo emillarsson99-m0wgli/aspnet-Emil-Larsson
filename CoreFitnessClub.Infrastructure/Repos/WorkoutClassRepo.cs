7440cde42a4a92cbee1052885adeeadbdab9408c
Emil L <emillarsson99@gmail.com>
2026-04-27 15:45:18 +0200

    Created infrastructure Repos for domain

---
+++ CoreFitnessClub.Infrastructure/Repos/WorkoutClassRepo.cs
@@ -0,0 +1,31 @@
+﻿using CoreFitnessClub.Application.Interfaces;
+using CoreFitnessClub.Domain.Entities;
+using CoreFitnessClub.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreFitnessClub.Infrastructure.Repos;
+
+public class WorkoutClassRepo : IWorkoutClassRepo
+{
+    private readonly ApplicationDbContext _context;
+
+    public WorkoutClassRepo(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<WorkoutClass>> GetAllAsync()
+    {
+        return await _context.WorkoutClasses
+            .OrderBy(x => x.Date)
+            .ThenBy(x => x.StartTime)
+            .ThenBy(x => x.Name)
+            .ToListAsync();
+    }
+
+    public async Task<WorkoutClass?> GetByIdAsync(int id)
+    {
+        return await _context.WorkoutClasses
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+}
