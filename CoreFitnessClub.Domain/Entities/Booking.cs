ec9226cd409cd59128acfe12cf59ee3dea957f5e
Emil L <emillarsson99@gmail.com>
2026-04-27 14:15:36 +0200

    Created Booking entity

---
+++ CoreFitnessClub.Domain/Entities/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreFitnessClub.Domain.Entities;
+
+public class Booking
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = null!;
+    public int WorkoutClassId { get; set; }
+    public DateTime BookedAt { get; set; } = DateTime.UtcNow;
+    public WorkoutClass WorkoutClass { get; set; } = null!;
+}
