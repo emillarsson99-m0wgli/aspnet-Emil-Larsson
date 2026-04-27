5b573eb51d9b98a31b10ff6937a57547f2fed03b
Emil L <emillarsson99@gmail.com>
2026-04-27 14:35:31 +0200

    Created IWorkoutClassService interface

---
+++ CoreFitnessClub.Application/Interfaces/IWorkoutClassService.cs
@@ -0,0 +1,9 @@
+﻿using CoreFitnessClub.Domain.Entities;
+
+namespace CoreFitnessClub.Application.Interfaces;
+
+public interface IWorkoutClassService
+{
+    Task<List<WorkoutClass>> GetWorkoutClassesAsync();
+    Task<WorkoutClass> GetWorkoutClassByIdAsync(int id);
+}
