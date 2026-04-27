d5e390052e933d5153d2d9ab3a32a510132f40b4
Emil L <emillarsson99@gmail.com>
2026-04-27 14:55:45 +0200

    Created WorkoutClassService

---
+++ CoreFitnessClub.Application/Services/WorkoutClassService.cs
@@ -0,0 +1,24 @@
+﻿using CoreFitnessClub.Application.Interfaces;
+using CoreFitnessClub.Domain.Entities;
+
+namespace CoreFitnessClub.Application.Services;
+
+public class WorkoutClassService
+{
+    private readonly IWorkoutClassRepo _workoutClassRepo;
+
+    public WorkoutClassService(IWorkoutClassRepo workoutClassRepo)
+    {
+        _workoutClassRepo = workoutClassRepo;
+    }
+
+    public async Task<List<WorkoutClass>> GetAllAsync()
+    {
+        return await _workoutClassRepo.GetAllAsync();
+    }
+
+    public async Task<WorkoutClass?> GetByIdAsync(int id)
+    {
+        return await _workoutClassRepo.GetByIdAsync(id);
+    }
+}
