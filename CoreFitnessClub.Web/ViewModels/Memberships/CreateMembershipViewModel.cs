a5489c15f4b1f6f4f583a18ef2e5753330e01729
Emil L <emillarsson99@gmail.com>
2026-04-29 15:24:57 +0200

    Created CreateMembershipViewmodel

---
+++ CoreFitnessClub.Web/ViewModels/Memberships/CreateMembershipViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreFitnessClub.Web.ViewModels.Memberships;
+
+public class CreateMembershipViewModel
+{
+    [Required(ErrorMessage = "Du måste välja medlemskapstyp.")]
+    [Display(Name = "Medlemskapstyp")]
+    public string Type { get; set; } = string.Empty;
+}
