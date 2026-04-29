830ca7229d134cd36de041f421df128e8aa7a2d3
Emil L <emillarsson99@gmail.com>
2026-04-29 16:17:25 +0200

    Created AccordionViewmodel

---
+++ CoreFitnessClub.Web/ViewModels/AccordionViewmodel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitnessClub.Web.ViewModels
+{
+    public class AccordionViewmodel
+    {
+        public int Id { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public List<string> Answers { get; set; } = new ();
+    }
+}
