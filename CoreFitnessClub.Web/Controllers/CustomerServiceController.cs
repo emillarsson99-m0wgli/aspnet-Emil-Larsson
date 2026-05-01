317f8150218e741d42977eb98c29b92c0707eb24
Emil L <emillarsson99@gmail.com>
2026-05-01 23:06:03 +0200

    Created CustomerServiceViewModel

---
+++ CoreFitnessClub.Web/Controllers/CustomerServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitnessClub.Web.Controllers
+{
+    public class CustomerServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreFitnessClub.Web/ViewModels/CustomerServiceViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitnessClub.Web.ViewModels;
+
+public class CustomerServiceViewModel
+{
+    public List<AccordionViewmodel> AccordionViews { get; set; } = new();
+}
