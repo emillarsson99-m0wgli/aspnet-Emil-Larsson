dea9b45cc18a55a79fbb341ad13288038eeb574b
Emil L <emillarsson99@gmail.com>
2026-05-01 18:57:50 +0200

    Created SetPasswordViewModel

---
+++ CoreFitnessClub.Web/ViewModels/Account/SetPasswordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreFitnessClub.Web.ViewModels.Account;
+
+public class SetPasswordViewModel
+{
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
