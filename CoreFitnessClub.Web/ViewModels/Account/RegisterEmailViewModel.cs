f5bbee111de6b1f6f8209d8b10573a18ecad4223
Emil L <emillarsson99@gmail.com>
2026-05-01 18:55:50 +0200

    Created RegisterEmailViewModel

---
+++ CoreFitnessClub.Web/ViewModels/Account/RegisterEmailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreFitnessClub.Web.ViewModels.Account;
+
+public class RegisterEmailViewModel
+{
+    [Required] 
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+}
