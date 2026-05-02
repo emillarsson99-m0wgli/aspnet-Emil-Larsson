31611fafcdd124c073105c84e56a1062a7a4adb6
Emil L <emillarsson99@gmail.com>
2026-05-03 00:28:03 +0200

    Created third BookingService unit test

--- CoreFitnessClub.Tests/Services/BookingServiceTests.cs
+++ CoreFitnessClub.Tests/Services/BookingServiceTests.cs
@@ -46,4 +46,24 @@ public class BookingServiceTests
         Assert.False(result);
         bookingRepo.Verify(x => x.AddAsync(It.IsAny<Booking>()), Times.Never);
     }
+
+    [Fact]
+    public async Task BookClassAsync_AddsBooking_WhenClassExistsAndNotAlreadyBooked()
+    {
+        var bookingRepo = new Mock<IBookingRepo>();
+        var workoutClassRepo = new Mock<IWorkoutClassRepo>();
+
+        workoutClassRepo.Setup(x => x.GetByIdAsync(1))
+            .ReturnsAsync(new WorkoutClass { Id = 1, Name = "Yoga" });
+
+        bookingRepo.Setup(x => x.ExistsAsync("user1", 1))
+            .ReturnsAsync(false);
+
+        var service = new BookingService(bookingRepo.Object, workoutClassRepo.Object);
+
+        var result = await service.BookClassAsync("user1", 1);
+
+        Assert.True(result);
+        bookingRepo.Verify(x => x.AddAsync(It.Is<Booking>(b => b.UserId == "user1" && b.WorkoutClass.Id == 1)), Times.Once);
+    }
 }
